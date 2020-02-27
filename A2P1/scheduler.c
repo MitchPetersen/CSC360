075812e1a0c522c36383c921d3baed7d835988d6
Mitch Petersen <mitchpet@uvic.ca>
2020-02-26 16:31:26 -0800

    try5

--- A2P1/scheduler.c
+++ A2P1/scheduler.c
@@ -103,7 +103,9 @@ void task_create(task_t* handle, task_fn_t fn) {
  * \param handle  This is the handle produced by task_create
  */
 void task_wait(task_t handle) {
-  wait(&handle);
+  pid_t pid;
+  pid = wait(&handle);
+  return pid;
 }
 
 /**
