9da9ad4797d4e2b4646f8160d44a3120e8997539
Mitch Petersen <mitchpet@uvic.ca>
2020-02-29 19:14:06 -0800

    new CSV

--- CSC360/A2P2/A2P2.c
+++ CSC360/A2P2/A2P2.c
@@ -126,7 +126,7 @@ int main(int argc, char** argv) {
 			tokens = str_split(lines, ',');
 			numbers[linenum-1] = atof(tokens[1]);
 			linenum++;
-			printf("%s", linenum);
+			printf("%d", linenum);
 		}
 	}
 	fclose(input);
