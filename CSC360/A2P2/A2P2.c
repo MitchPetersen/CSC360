6961ef7eafb4be966bc4799fd689646e2222bf32
Mitch Petersen <mitchpet@uvic.ca>
2020-02-29 18:32:26 -0800

    full first implement

--- CSC360/A2P2/A2P2.c
+++ CSC360/A2P2/A2P2.c
@@ -5,9 +5,55 @@
 #include <assert.h>
 extern char* strdup(const char*);
 
+float numbers[4000];
+int datapoint_size;
+int num_threads = 4;
 
-void func(int arr[], int val) {
-	
+struct line {
+	float intercept;
+	float slope;
+	float SAR;
+} line;
+
+line bestline[4]
+
+float slope_calc(float y1, float y2, float x1, float x2){
+	float slope = (y2-y1)/(x2-x1);
+	return slope;
+}
+
+float intercept_calc(float slope, float x, float y){
+	float intercept = y-(slope*x);
+	return intercept;
+}
+
+float dist(line line_obj, float y, float x){
+	float p2 = (line_obj.slope*x)+line_obj.intercept;
+	float dist = p2-y;
+	dist = fabs(dist);
+	return dist;
+}
+
+void *func(void* i) {
+	int index = *(int *) i;
+	float minimum_distance = FLT_MAX;
+	datapoint_size = sizeof(numbers)/sizeof(float);
+	for(int i = index; i<datapoint_size; i+num_threads){
+		for(int j = index+1; j<datapoint_size; j++){
+			line line_obj;
+			line_obj.slope = slope_calc(numbers[i], numbers[j], i, j);
+			line_obj.intercept = intercept_calc(line_obj.slope, i, j);
+			float total_distance=0;
+			for(int k=0; k<datapoint_size; k++){
+				total_distance += dist(line_obj, numbers[k], k);
+			}
+			if (total_distance<minimum_distance){
+				minimum_distance = total_distance;
+				line_obj.SAR=minimum_distance;
+				bestline[i] = line_obj;
+			}
+		}
+	}
 }
 
 char** str_split(char* a_str, const char a_delim)
@@ -61,7 +107,7 @@ char** str_split(char* a_str, const char a_delim)
 int main(int argc, char** argv) {
 	FILE *input;
 	input = fopen(argv[1], "r");
-	float numbers[4000];
+	
 	char** tokens;
 	if (input == NULL) {
 		fprintf(stderr, "problem reading file\n");
@@ -79,7 +125,23 @@ int main(int argc, char** argv) {
 			linenum++;
 		}
 	}
-	
 	fclose(input);
+	pthread_t thread_arr[num_threads];
+	for (int i=0; i<num_threads; i++){
+		pthread_create(&thread_arr[i], NULL, func, &i);
+	}
+	for (int i=0; i<num_threads; i++){
+		pthread_join(thread_arr[i], NULL);
+	}
+	line best;
+	float sar = FLT_MAX;
+	for (int i=0; i<num_threads; i++){
+		if(bestline[i].SAR<sar){
+			sar = bestline[i].sar;
+			best = bestline[i];
+		}
+	}
+	printf("Best Line:\nSlope: %f\nIntercept: %f\nSAR: %f\n", best.slope, best.intercept, best.SAR);
+	
 	return 0;
 }
--- CSC360/A2P2/readme.txt
+++ CSC360/A2P2/readme.txt
@@ -1,1 +1,3 @@
 str_split obtained here: https://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
+
+Worked with Kelly and Savio.
