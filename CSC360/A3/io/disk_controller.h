ff493eb1326123a2cd4009adb92f2d6eab12a838
Mitch Petersen <mitchpet@uvic.ca>
2020-03-30 16:02:47 -0700

    first commit

--- CSC360/A3/io/disk_controller.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISK_CONTROLLER
-#define DISK_CONTROLLER
-
-FILE* open_file(char* mode);
-void create_vdisk();
-void write_to_disk(int block_index, char* buffer);
-char* read_from_disk(int block_index);
-
-#endif
