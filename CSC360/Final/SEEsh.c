0c685459cc4502e712fb83154061982e503b396c
Mitch Petersen <mitchpet@uvic.ca>
2020-04-20 14:19:30 -0700

    push

---
+++ CSC360/Final/SEEsh.c
@@ -0,0 +1,23 @@
+/***************************************************************************//**
+  @file         fork.c
+  @author       Mitch Petersen, V00845204
+*******************************************************************************/
+
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+int main(int argc, char **argv) {
+	FILE *out = fopen("output.txt", "w");
+
+	fork()
+	fork()
+	fork()
+
+	pid_t pid = getpid()
+	pid_t ppid = getppid()
+	
+	fprintf(out, "this process is %lun and it's parent is %lun", pid, ppid);
+
+	return EXIT_SUCCESS;
+}
