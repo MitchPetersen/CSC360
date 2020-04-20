3e740a03cdad9cd82417e59124fb1747939ddfa8
Mitch Petersen <mitchpet@uvic.ca>
2020-04-20 14:32:20 -0700

    test

--- CSC370/Final/fork.c
+++ CSC370/Final/fork.c
@@ -1,5 +1,5 @@
 /***************************************************************************//**
-  @file         fork.c
+  @file         processes.c
   @author       Mitch Petersen, V00845204
 *******************************************************************************/
 
@@ -17,5 +17,5 @@ int main(int argc, char **argv) {
 	pid_t pid = getpid();
 	pid_t ppid = getppid();
 	
-	fprintf(out, "this process is %lun and it's parent is %lun\n", pid, ppid);
+	fprintf(out, "this process is %u and it's parent is %u\n", pid, ppid);
 }
