0f015e23cd187993589a4bdfc84d8abdf44862e1
Mitch Petersen <mitchpet@uvic.ca>
2020-04-20 15:37:38 -0700

    test

--- CSC370/Final/disksched.c
+++ CSC370/Final/disksched.c
@@ -10,7 +10,7 @@ int main()
 {
 	int arr[15] = {19, 73, 2, 15, 16, 180, 12, 65, 34, 67, 34, 82, 13, 42, 102};
 	
-	printf("FCFS, start position 0");
+	printf("FCFS, start position 0\n");
 	int seek = 0;
 	for (int i=0; i<15; i++) {
 		printf("Servicing %i\n", arr[i]);
@@ -20,5 +20,18 @@ int main()
 			seek += arr[i];
 		}
 	}
+	printf("Seek time of %i\n\n", seek);
 	
+	printf("SSTF, start position 0\n");
+	seek = 0;
+	int arr[15] = {2,12,13,15,16,19,34,34,42,65,67,73,82,102,180}
+	for (int i=0; i<15; i++) {
+		printf("Servicing %i\n", arr[i]);
+		if(i>=1) {
+			seek += abs(arr[i]-arr[i-1]);
+		} else {
+			seek += arr[i];
+		}
+	}
+	printf("Seek time of %i\n\n", seek);
 }
