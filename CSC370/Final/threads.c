0d480c6b41eb36b9c6e6996ceb2faa8e7fb8a85d
Mitch Petersen <mitchpet@uvic.ca>
2020-04-20 14:41:56 -0700

    test

--- CSC370/Final/threads.c
+++ CSC370/Final/threads.c
@@ -1,26 +1,16 @@
-/**
- * This program forks a separate process using the fork()/exec() system calls.
- *
- * Figure 3.08
- *
- * @author Silberschatz, Galvin, and Gagne
- * Operating System Concepts  - Tenth Edition
- * Copyright John Wiley & Sons - 2018
- */
-
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h>
 #include <pthread.h> 
 
-void *mythread() {
+void *myread() {
 	execlp("/bin/ls","ls",NULL);
 }
 
 int main()
 {
 	pthread_t th_id;
-	pthread_create(&th_id, NULL, mythread, NULL);
+	pthread_create(&th_id, NULL, myread, NULL);
 	pthread_join(th_id, NULL);
 	
     return 0;
