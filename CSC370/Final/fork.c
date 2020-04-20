1a3fe3ef5213126e80e4b6e64ba6f3e840c034c8
Mitch Petersen <mitchpet@uvic.ca>
2020-04-20 14:28:57 -0700

    test

--- CSC370/Final/fork.c
+++ CSC370/Final/fork.c
@@ -10,14 +10,14 @@
 int main(int argc, char **argv) {
 	FILE *out = fopen("output.txt", "w");
 
-	fork()
-	fork()
-	fork()
+	fork();
+	fork();
+	fork();
 
 	pid_t pid = getpid()
 	pid_t ppid = getppid()
 	
 	fprintf(out, "this process is %lun and it's parent is %lun\n", pid, ppid);
 
-	return EXIT_SUCCESS;
+	return;
 }
