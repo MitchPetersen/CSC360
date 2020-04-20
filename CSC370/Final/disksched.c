c71fb0cfaa4bce8ba24ba37ee4c283cd3bec695b
Mitch Petersen <mitchpet@uvic.ca>
2020-04-20 16:26:32 -0700

    test

--- CSC370/Final/disksched.c
+++ CSC370/Final/disksched.c
@@ -8,9 +8,13 @@
 
 int main()
 {
-	int arr[15] = {19, 73, 2, 15, 16, 180, 12, 65, 34, 67, 34, 82, 13, 42, 102};
 	
-	printf("FCFS, start position 0\n");
+	int arr[15] = {19, 73, 2, 15, 16, 180, 12, 65, 34, 67, 34, 82, 13, 42, 102};
+	int loop(loop = 0; loop < 15; loop++){
+		printf(%d, ", arr[loop];
+	}
+	
+	printf("\nFCFS, start position 0\n");
 	int seek = 0;
 	for (int i=0; i<15; i++) {
 		printf("Servicing %i\n", arr[i]);
@@ -24,7 +28,12 @@ int main()
 	
 	printf("SSTF, start position 0\n");
 	seek = 0;
+	
+	
 	int arr2[15] = {2,12,13,15,16,19,34,34,42,65,67,73,82,102,180};
+	// as we are starting at position 0, the smallest seek times are going to be just the entire array sorted into smallest to largest
+	// I took the liberty of hard coding it to save time. 
+	
 	for (int j=0; j<15; j++) {
 		printf("Servicing %i\n", arr2[j]);
 		if(j>=1) {
