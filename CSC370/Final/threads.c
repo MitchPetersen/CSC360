ecc6b7a72fca42ae03880aadac4ff7da1070a246
Mitch Petersen <mitchpet@uvic.ca>
2020-04-20 14:49:20 -0700

    test

--- CSC370/Final/threads.c
+++ CSC370/Final/threads.c
@@ -2,17 +2,20 @@
 #include <stdlib.h> 
 #include <unistd.h>
 #include <pthread.h> 
+#include <dirent.h>
+
+struct dirent *readdir(DIR *dirp);
 
 void *myread() {
-	execlp("/bin/ls","ls",NULL);
+	readdir();
 }
 
 int main()
 {
-	printf("before thread");
+	printf("before thread\n");
 	pthread_t th_id;
 	pthread_create(&th_id, NULL, myread, NULL);
 	pthread_join(th_id, NULL);
-	printf("after thread");
+	printf("after thread\n");
     return 0;
 }
